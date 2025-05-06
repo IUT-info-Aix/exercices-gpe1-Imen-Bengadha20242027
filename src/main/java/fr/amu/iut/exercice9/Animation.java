058cd17bffc48cb37fc090929f7210df72c36def
Ben gadha <imen.BEN-GADHA@etu.univ-amu.fr>
2025-05-06 17:04:08 +0200

    TP3

--- src/main/java/fr/amu/iut/exercice9/Animation.java
+++ src/main/java/fr/amu/iut/exercice9/Animation.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import javafx.animation.SequentialTransition;
 
 public class Animation extends Application {
 
@@ -18,10 +19,53 @@ public class Animation extends Application {
 
         Duration duration = Duration.millis(1500);
         TranslateTransition transition1 = new TranslateTransition(duration, customButton);
-        transition1.setByX(150);
         transition1.setByY(-150);
         transition1.setAutoReverse(true);
-        transition1.setCycleCount(2);
+        transition1.setCycleCount(1);
+        TranslateTransition transitionGauche = new TranslateTransition(duration, customButton);
+        transitionGauche.setByX(150);
+        TranslateTransition transitionD = new TranslateTransition(duration, customButton);
+        transitionD.setByY(300);
+        TranslateTransition transitionG = new TranslateTransition(duration, customButton);
+        transitionG.setByX(-300);
+        TranslateTransition transitionH = new TranslateTransition(duration, customButton);
+        transitionH.setByY(-300);
+        TranslateTransition transitionD1 = new TranslateTransition(duration, customButton);
+        transitionD1.setByX(150);
+        TranslateTransition transitionB = new TranslateTransition(duration, customButton);
+        transitionB.setByY(150);
+
+        SequentialTransition sequence = new SequentialTransition(transition1, transitionGauche, transitionD, transitionG, transitionH, transitionD1, transitionB);
+        sequence.play();
+        // Transitions en sens inverse (mêmes durées, mêmes objets, sens opposé)
+        TranslateTransition inverseB = new TranslateTransition(duration, customButton);
+        inverseB.setByY(-150);
+
+        TranslateTransition inverseD1 = new TranslateTransition(duration, customButton);
+        inverseD1.setByX(-150);
+
+        TranslateTransition inverseH = new TranslateTransition(duration, customButton);
+        inverseH.setByY(300);
+
+        TranslateTransition inverseG = new TranslateTransition(duration, customButton);
+        inverseG.setByX(300);
+
+        TranslateTransition inverseD = new TranslateTransition(duration, customButton);
+        inverseD.setByY(-300);
+
+        TranslateTransition inverseGauche = new TranslateTransition(duration, customButton);
+        inverseGauche.setByX(-150);
+
+        TranslateTransition inverse1 = new TranslateTransition(duration, customButton);
+        inverse1.setByY(150);
+        SequentialTransition retour = new SequentialTransition(
+                inverseB, inverseD1, inverseH, inverseG, inverseD, inverseGauche, inverse1
+        );
+        sequence.setOnFinished(e -> retour.play());
+
+
+
+
 
 //        SequentialTransition st = new SequentialTransition(transition1, transition2, transition3, transition4, transition5);
 
